0a631b99501b76e5702df7d37ee0a90ef58617e0
taher19999 <94671425+taher19999@users.noreply.github.com>
2023-03-04 15:45:09 +0300

    Update and rename config.py.sample to config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 
 # Token del bot de Telegram
 # https://core.telegram.org/bots#how-do-i-create-a-bot
-telegram_token="XXXX"
+telegram_token="5857390887:AAEaf7YKD08h5z01vc3lrsqBckohMxQmNnU"
 # Los IDs de telegram de los usuarios que responderá el bot
 # Talk to @userinfobot bot on telegram to get yours
-telegram_users=[XXXX]
+telegram_users=[5198110160]
 
 # Activar o no la opción de envío de notas de voz
 voice_enabled = True
@@ -16,8 +16,8 @@ voice_enabled = True
 openai_mode = "api"
 
 # Usuario y contraseña de OpenIA (no funcional ya)
-openai_email = "user@email.com"
-openai_password = "XXXX"
+openai_email = "electricalengineeringdev@gmail.com"
+openai_password = "taherdev1999&%@"
 # Cookie y token de sesión
 openai_session = "XXXX"
 openai_clearance = "XXXX"
@@ -25,7 +25,7 @@ openai_useragent = "XXXX"
 
 # API de OpenAI (solo si el modo de openai es 'api')
 # https://beta.openai.com/account/api-keys
-openai_api_key = "XXXX"
+openai_api_key = "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"
 
 # Claves y servidor de AWS para Amazon polly
 # https://us-east-1.console.aws.amazon.com/iam/home?region=us-east-1#/security_credentials
@@ -36,4 +36,4 @@ region_name="eu-west-3"
 # Configuración y API de Stability.ia
 sd_engine_id = "stable-diffusion-512-v2-0"
 sd_api_host = "https://api.stability.ai"
-sd_api_key = "XXXX"
+sd_api_key = "sk-aNay2jF9UCqG3MBvuG3CIudvK6ES3CWBDnRzcLzF3w93VWY7"
